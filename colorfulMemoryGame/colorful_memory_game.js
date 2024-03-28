3029e25a2e79c93d54f32e2e82452872c7a2115c
eelnevs <mwl82701@gmail.com>
2024-03-28 14:14:31 -0500

    improve memory game. No self match.

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -5,6 +5,7 @@ let score = 0;
 let gameInterval;
 const totalTime = 60;
 let timeLeft = totalTime;
+let totalMatched = 0;
 
 const startbtn = document.getElementById('startbtn');
 const resetbtn = document.getElementById('resetbtn');
@@ -30,42 +31,46 @@ function shuffle(array) {
     return array;
 }
 
-function handleCardClick(event) {
+async function handleCardClick(event) {
     const card = event.target;
-    if (!card.classList.contains('card') || card.classList.contains('matched')) {
+    if (!card.classList.contains('card') || card.classList.contains('matched') || card.classList.contains('flipped')) {
         return;
     }
+    card.classList.add('flipped');
     card.textContent = card.dataset.color;
     card.style.backgroundColor = card.dataset.color;
     selectedCards.push(card);
     if (selectedCards.length === 2) {
-        setTimeout(checkMatch, 500);
+        setTimeout(checkMatch, 300);
     }
 }
 
 function checkMatch() {
-    gameContainer.disabled = true;
+    gameContainer.removeEventListener('click', handleCardClick)
     const [card1, card2] = selectedCards;
     if (card1.dataset.color === card2.dataset.color) {
         card1.classList.add('matched');
         card2.classList.add('matched');
         score += 2;
         scoreElement.textContent = `Score: ${score}`;
+        totalMatched++;
     } else {
         card1.textContent = '?';
         card2.textContent = '?';
         card1.style.backgroundColor = '#ddd';
         card2.style.backgroundColor = '#ddd';
+        card1.classList.remove('flipped');
+        card2.classList.remove('flipped');
     }
     selectedCards = [];
-    gameContainer.disabled = false;
-    if (score == 24) {
+    if (totalMatched == 24) {
         gameComplete();
     }
+    gameContainer.addEventListener('click', handleCardClick);
 }
 
 function startGame() {
-    let timeLeft = totalTime;
+    timeLeft = totalTime;
     startbtn.textContent = "Restart";
     startbtn.classList.toggle('Start');
     scoreElement.textContent = `Score: ${score}`;
@@ -92,12 +97,15 @@ function startGameTimer(timeLeft) {
 
 function reset(scoreReset = false) {
     clearInterval(gameInterval);
-    let timeLeft = totalTime;
     startbtn.textContent = "Start";
     startbtn.classList.toggle('Start');
     timerElement.textContent = `Time Left: ${totalTime}`;
     gameContainer.innerHTML = "";
-    if (scoreReset) score = 0;
+    totalMatched = 0;
+    if (scoreReset) {
+        score = 0;
+        scoreElement.textContent = `Score: ${score}`;
+    }
 }
 
 function gameComplete() {
